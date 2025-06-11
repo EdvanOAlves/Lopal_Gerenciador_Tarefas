3c71e4518043136443d48252b964e0b97106ef30
EDVAN ALVES DE OLIVEIRA <25132698@dsn127m1242316.cfp127aluno.local>
2025-06-11 10:29:48 -0300

    Adicionada Factory para tarefas

---
+++ src/br/dev/edvan/gerenciador_tarefas/factory/ArquivoTarefaFactory.java
@@ -0,0 +1,33 @@
+package br.dev.edvan.gerenciador_tarefas.factory;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ArquivoTarefaFactory {
+	private String caminho = "/Users/25132698/projetoTarefas/tarefas.csv";
+	private FileWriter fw;
+	private BufferedWriter bw;
+	private FileReader fr;
+	private BufferedReader br;
+	
+	public BufferedWriter getBw() throws IOException {
+		
+		fw = new FileWriter(caminho, true);//ditando o caminho e avisando o programa que vai ser append true
+		bw = new BufferedWriter(fw);
+		
+		return bw;
+	}
+	
+	public BufferedReader getBr() throws FileNotFoundException {
+		
+		fr = new FileReader(caminho);
+		br = new BufferedReader(fr);
+		return br;
+		
+	}
+
+}
